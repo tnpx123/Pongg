d26fc1ae613e3c7a29fcacb9b24e9500340aeeb5
tnpx123 <robertgaw26@gmail.com>
2025-05-09 13:04:59 +0100

    add moving ball

--- src/Ball.java
+++ src/Ball.java
@@ -1,8 +1,11 @@
+import java.util.Random;
+
 import static utils.Constants.*;
 
 public class Ball extends Sprite{
     private double vx;
     private double vy;
+    Random r= new Random();
 
     public Ball() {
         super(BALL_IMAGE_PATH,295,215 , BALL_WIDTH, BALL_HEIGHT);
@@ -10,15 +13,27 @@ public class Ball extends Sprite{
     }
 
     public void resetBall(){
+        int r1 = r.nextInt(4);
+        if (r1 == 0){
+            vx = -BALL_SPEED;
+            vy = -BALL_SPEED;
+        } else if (r1 == 1) {
+            vx = BALL_SPEED;
+            vy = BALL_SPEED;
+        } else if (r1 == 2) {
+            vx = BALL_SPEED;
+            vy = -BALL_SPEED;
+        } else{
+            vx = -BALL_SPEED;
+            vy = BALL_SPEED;
+        }
         //position ball to centre of screen
-        //reset ball velocit
+        //reset ball velocity
     }
 
     @Override
     public void tick() {
         getPos().translate((int) vx,(int) vy);
-        vx = BALL_SPEED;
-        vy = BALL_SPEED;
         pos.x = Math.clamp(pos.x, 0, BOARD_WIDTH - BALL_WIDTH);
         pos.y = Math.clamp(pos.y, 0, BOARD_HEIGHT - BALL_HEIGHT);
     }
